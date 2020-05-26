8549268f0f431d54cb0f2f19ecb1fdf1b68b0b7b
aldidwikip <aldidwiki@gmail.com>
2020-05-26 15:55:39 +0700

    update routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,8 +49,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['api/kontak'] = 'Kontak';
-$route['api/kontak_android'] = 'Kontak_android';
+$route['api/kontak'] = 'api/kontak/kontak';
+$route['api/kontak_android'] = 'api/kontak/kontak_android';
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
