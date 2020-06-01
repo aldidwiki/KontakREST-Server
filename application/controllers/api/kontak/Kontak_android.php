f87d56009b1be10babec463fc35c4d443558e9d8
aldidwikip <aldidwiki@gmail.com>
2020-06-01 18:33:50 +0700

    add search function

--- application/controllers/api/kontak/Kontak_android.php
+++ application/controllers/api/kontak/Kontak_android.php
@@ -15,18 +15,23 @@ class Kontak_android extends REST_Controller {
 
     //Menampilkan data kontak
     function index_get() {
-        $this->db->order_by('nama','ASC');
+        $this->db->order_by('nama', 'ASC');
         $kontak = $this->db->get('telepon')->result();
         $this->response(array("result" => $kontak, 200));
     }
 
+    //Fungsi Search
+    function index_post() {
+        $keyword = $this->post('keyword');
+
+        if (!empty($keyword)) {
+            $this->db->like('nama', $keyword);
+            $search = $this->db->get('telepon')->result();
+            $this->response(array("result" => $search, 200));
+        } else {
+            $this->response(array("no result"), 200);
+        }
+    }
+
 }
-?>
-
-
-
-
-
-
-
 
